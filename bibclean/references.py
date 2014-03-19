6b4fb00459b6428c67185a927064b95d5ace2eb2
Alex J. Grede <agrede@gmail.com>
2014-03-19 06:39:46 +0000

    removed error checking (for now), better contributor management

--- bibclean/references.py
+++ bibclean/references.py
@@ -16,7 +16,8 @@ class References(object):
         self.dbc = zotero.Zotero(dbc_params['library_id'],
                                  'user', dbc_params['api_key'])
         self.dbc_params = dbc_params
-        self._items = []
+        self.items = []
+        self.people = {}
 
     def get_all(self):
         items_raw = self._dbc.everything(self._dbc.top())
@@ -24,7 +25,15 @@ class References(object):
             self.add_item(self, item_raw)
 
     def add_item(self, raw):
-        self._items.append(Item(self, raw))
+        self.items.append(Item(self, raw))
+
+    def get_person(self, name):
+        if not name[0] in self.people:
+            self.people[name[0]] = {}
+        if name[1] in self.people[name[0]]:
+            return self.people[name[0]][name[1]]
+        else:
+            self.people[name[0]][name[1]] = Person(name)
 
 
 class Item(object):
@@ -34,7 +43,7 @@ class Item(object):
     def __init__(self, parent, raw):
         self._raw = raw
         self.parent = weakref.ref(parent)
-        self.people = []
+        self.contributors = []
 
     def update(self):
         if self.parent.dbc.check_items([self._raw]):
@@ -60,16 +69,14 @@ class Item(object):
         else:
             return None
 
-    def _add_people(self):
+    def _add_contributor(self):
         if 'creators' in self._raw:
             for creator in self._raw['creators']:
                 if 'firstName' in creator and 'lastName' in creator:
-                    self._people.append(Person(self,
-                                               creator['firstName'],
-                                               creator['lastName']))
-
-    def people(self):
-        return self._people
+                    name = [creator['lastName'], creator['firstName']]
+                    person = self.parent.get_person(name)
+                    self.contributors.append(
+                        person.add_contribution(self, name))
 
     @property
     def url(self):
@@ -103,29 +110,11 @@ class Item(object):
     def date(self, value):
         self._update_field('date', value)
 
-    def update_person(self, idx, name):
+    def update_contributor(self, idx, name):
         if 'creators' in self._raw:
-            tmp = self._raw['creators'][idx]
             self._raw['creators'][idx]['lastName'] = name[0]
             self._raw['creators'][idx]['firstName'] = name[1]
-            try:
-                return self.update()
-            except:
-                self._raw['creators'][idx] = tmp
-                raise
-                return False
-        return False
-
-
-class People(object):
-    """
-    People
-    """
-    def __init__(self, parent):
-        self.parent = weakref.ref(parent)
-
-    def __iter__(self):
-        return self
+            self.update()
 
 
 class Person(object):
@@ -133,33 +122,31 @@ class Person(object):
     Person
     """
     def __init__(self, name):
-        self._contributions = []
+        self.contributions = []
         self._name = name
 
     def ascii_name(self):
         return name_to_ascii(self._name)
 
-    def contributions(self):
-        return self._contributions
-
     def add_contribution(self, item, name):
-        tmp = Contributer(item, self, name)
+        contrib = Contributer(item, self, name)
         if self._name != name:
-            try:
-                tmp.name = self._name
-            except:
-                raise
-                return False
-        self._contributions.append(tmp)
-        return True
+            contrib.name = self._name
+        self.contributions.append(contrib)
+        return contrib
 
     @property
     def name(self):
         return self._name
 
+    @name.setter
+    def name(self, value):
+        for c in self.contributions:
+            if c.name != value:
+                c.name = value
+
     def __str__(self):
         return print(', '.join(self._name))
-
 
 
 class Contributer(object):
@@ -180,9 +167,13 @@ class Contributer(object):
 
     @name.setter
     def name(self, value):
-        idx = self.parent.index(self)
-        try:
-            if self.parent.update_person(idx, value):
-                self._name = value
-        except:
-            raise
+        idx = self.item.index(self)
+        if self._name != value:
+            self.item.update_contributor(idx, value)
+            self._name = value
+
+    def change_person(self, person):
+        self.person.contributions.remove(self)
+        if person.name != self.name:
+            self.name = person.name
+        self.person = weakref.ref(person)
